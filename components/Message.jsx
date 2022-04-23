a2ce1ff9988653879ae159d22d3e8a424839b114
Suleiman Sulaimanov <sulaymanov.suleyman@gmail.com>
2022-04-23 18:58:18 -0500

    safe comit

--- components/Message.jsx
+++ components/Message.jsx
@@ -3,14 +3,20 @@ import css from '../styles/Home.module.css'
 const Message = ({sender, text}) => {
     const prefix=''
     if(sender=='u'){
-        prefix='Astronaout:  '
+        prefix='Astronaout: '
     }else{
-        prefix='You:   '
+        prefix='You: '
     }
     return (
+        <div className={css.msgWrapper}>
         <div className={sender=='u'?css.textMessageU:css.textMessageA}>
-            {prefix+text}
+            {prefix}
         </div>
+        <div className={css.msg}>
+            {text}
+        </div>
+        </div>
+
     )
 }
 
