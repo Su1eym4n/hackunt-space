d8a7210daf3b80371eb78d21cd0511bf56b187c5
zsc0012 <zacharycarpenter@my.unt.edu>
2022-04-23 18:57:46 -0500

    Work on planet rotation

--- pages/chat.js
+++ pages/chat.js
@@ -4,6 +4,7 @@ import css from "../styles/Chat.module.css"
 import LightBulb from '../components/LightBulb'
 import OrbitControls from '../components/OrbitControls'
 import ChatPlanet from '../components/ChatPlanet'
+import Button from '@mui/material/Button'
 
 import React, { useState } from 'react';
 
@@ -11,12 +12,23 @@ import React, { useState } from 'react';
 export default function Chat() {
 
     const stage1 = {
-        rotateAmount: 0.001,
-        nextProp: 0
+        rotateAmount: 0.01,
+        planetX: 5,
+        planetY: 6
+
     }
     const stage2 = {
-        rotateAmount: 0.00
+        rotateAmount: 0.00,
+        planetX: 0,
+        planetY: 0
     }
+
+
+
+
+    
+
+
 
     const [currentStage, setStage] = useState(stage1)
 
@@ -26,7 +38,11 @@ export default function Chat() {
 
                 <div className={css.history}>
                     History here
+                    <Button onClick={ (event) => setStage(stage2) }>Click Me</Button>
+                    <Button onClick={ (event) => setStage(stage1) }>stage1</Button>
                 </div>
+
+        
 
                 <div className={css.scene}>
                     <Canvas
@@ -38,7 +54,7 @@ export default function Chat() {
                     >
                         <ambientLight color={"white"} intensity={0.2} />
                         <LightBulb position={[0, 5, 3]} />
-                        <ChatPlanet rotateX={5} rotateY={5} rotateAmount={currentStage.rotateAmount} />
+                        <ChatPlanet rotateToX={currentStage.planetX} rotateYamt={currentStage.planetY} rotateAmount={currentStage.rotateAmount} />
                         {/* <OrbitControls/> */}
                     </Canvas>
                 </div>
