cb3964c3efde5ff6e864daa97ed2f47fdbc82ac1
Suleiman Sulaimanov <sulaymanov.suleyman@gmail.com>
2022-04-24 01:21:35 -0500

    a

--- components/Choice.jsx
+++ components/Choice.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import css from '../styles/Home.module.css'
 const Choice = ({ text }) => {
+
+
     const decide = (event)=>{
         event.preventDefault()
         console.log(text)
